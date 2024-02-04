e8e9adf0c90c4b48bb5cabe8ad1506d383d5cce7
Nadeem <nadeemamdev@gmail.com>
2024-02-04 12:17:58 +0000

    Add functionality by editing index.js, package.json and generateMarkdown.js.

--- challenge/starter/utils/generateMarkdown.js
+++ challenge/starter/utils/generateMarkdown.js
@@ -1,8 +1,68 @@
-// function to generate markdown for README
+// Function to generate markdown content for README based on user's responses.
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
-}
-
-module.exports = generateMarkdown;
+  let badgeLicense;
+  switch (data.license) {    
+  case ('MIT License'):
+  badgeLicense='![License](https://img.shields.io/badge/License-MIT-yellow.svg)';
+  break;
+  case ('GNU GPLv3'):
+  badgeLicense='![License](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+  break;
+  case ('Apache License'):
+  badgeLicense='![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
+  break;
+  default:
+  badgeLicense='';
+  }
+  
+  return `
+  # ${data.title} ${badgeLicense}
+  
+  ## Description 
+  
+  ${data.description}
+  
+  ## Table of Contents 
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation 
+  
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+  
+  ## Usage 
+  
+  ${data.usage}
+  
+  ## Contributing 
+  
+  ${data.contributing}
+  
+  
+  ## Tests 
+  
+  \`\`\`
+  ${data.tests}
+  \`\`\`
+  
+  
+  ---
+  
+  For any inquiries or issues, please contact through following channels:
+  
+  GitHub Profile Link - [@${data.githubUsername}](https://github.com/${data.githubUsername})
+  
+  Email Address - ${data.email}
+  `;
+  };
+  
+  module.exports = generateMarkdown;
+  
+  
